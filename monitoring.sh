3644b9287dc4ccaefa8095bfc45f5ec44ae16135
Mostafa <mos.abdallah@yahoo.com>
2025-01-27 19:16:02 +0200

    Fixing Bugs

--- monitoring.sh
+++ monitoring.sh
@@ -3,12 +3,13 @@ set -e
 echo "System Monitoring Alert - `date +"%Y-%m-%d %H:%M:%S"`\n"
 echo "================================================================\n"
 echo "Disk Usage: "
-df 
+echo "`df`\n" 
 usage_list=$( df | awk 'NR > 1 {print $5}' )
 index=1
 for i in $usage_list;do
 	if [ ${i%"%"} -ge 80 ];then
-		echo -e "\e[30mWarning : `df | awk -v ch_index=${index} 'NR == "ch_index" {print $1}'` is above 80% Usage\e[0m"
+		text=`df | awk -v ch_index=${index} 'NR == ch_index {print $1}'`
+		echo "Warning : ${text} is above 80% Usage"
 	fi
 	index=$(( index + 1 ))
 done
@@ -21,5 +22,6 @@ echo "\nMemory Usage"
 echo "Total Memory: `free -g | awk 'NR == 2 {print $2}'`GB"
 echo "Free Memory: `free -g | awk 'NR == 2 {print $4}'`GB"
 echo "Used Memory: `free -g | awk 'NR == 2 {print $3}'`GB"
-echo "\n top 5 Memory-consuming Processes: "
+echo "\nTop 5 Memory-consuming Processes: "
 ps -eo user,pid,%mem,command --sort=%mem | head -n 6
+
