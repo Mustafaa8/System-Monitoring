a260872d7f6f941c5b15a55f818f0a89345a4e11
Mostafa <mos.abdallah@yahoo.com>
2025-01-27 17:57:41 +0200

    Red Color Warning

--- monitoring.sh
+++ monitoring.sh
@@ -8,7 +8,7 @@ usage_list=$( df | awk 'NR > 1 {print $5}' )
 index=1
 for i in $usage_list;do
 	if [ ${i%"%"} -ge 80 ];then
-		echo "Warning : `df | awk -v ch_index=${index} 'NR == "ch_index" {print $1}'` is above 80% Usage"
+		echo -e "\e[30mWarning : `df | awk -v ch_index=${index} 'NR == "ch_index" {print $1}'` is above 80% Usage\e[0m"
 	fi
 	index=$(( index + 1 ))
 done
