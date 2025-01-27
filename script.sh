26da4d08c2b392e6792533796ca736b2bc98a026
Mostafa <mos.abdallah@yahoo.com>
2025-01-27 17:34:33 +0200

    Disk Usage

--- script.sh
+++ script.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -e
-
+echo "System Monitoring Alert - `date +"%Y-%m-%d %H:%M:%S"`\n"
+echo "================================================================\n"
+echo "Disk Usage: \n"
 df 
-usage_list=$( df | awk '{print $5}' )
-index=0
-for i in "$usage_list"
-do
-	echo $i
-	if [[ $i -ge 80 ]]
-	then
-		echo "Warning : $( df | awk -v index=${index} 'NR==index {print $1}' ) is above 80% Usage"
+usage_list=$( df | awk 'NR > 1 {print $5}' )
+index=1
+for i in $usage_list;do
+	if [ ${i%"%"} -ge 80 ];then
+		echo "Warning : `df | awk -v ch_index=${index} 'NR == "ch_index" {print $1}'` is above 80% Usage"
 	fi
-	index= $(( index+1 ))
+	index=$(( index + 1 ))
 done
+
+echo "CPU Usage: \n"
+
